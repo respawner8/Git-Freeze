ff0bdeaea821d400fe754511bad422dab9798b0b
respawner <nikhil8.anand@gmail.com>
2019-12-20 19:34:06 +0530

    directory changed

--- Week-1/Nikhil/web_scraper.js
+++ Week-1/Nikhil/web_scraper.js
@@ -2,17 +2,17 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 
-request('https://www.flipkart.com/search?q=redmi&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off', (error,response, html)=>{
+request('https://www.gadgetsnow.com/compare-mobile-phones/Apple-iPhone-11-Pro-vs-OnePlus-7T', (error,response, html)=>{
     if(!error && response.statusCode == 200){
         const $ = cheerio.load(html);
 
-        const productName = $('._2cLu-l');
+        // const productName = $('._2cLu-l');
 
         // console.log(productName.text());
 
-        $('._3liAhj').each((i, el)=>{
+        $('.inr_tbl').each((i, el)=>{
             const item = $(el)
-            .find('._2cLu-l')
+            .find('.ul2')
             .text();
 
             console.log(item);
